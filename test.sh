d6e34049679fbd17642d030bd24b50d7e0a75d55
Francesco Bianco <info.francescobianco@gmail.com>
2019-12-10 01:34:23 +0100

    small fix

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ source lcov.sh "$@"
 
 lcov_init *.sh !test.sh !release.sh !coverage
 
-run_test ./test/lcov_init.sh ./test/lcov_init2.sh
-run_test ./test/lcov_init3.sh
+run_test ./test/lcov_init.sh
 
 lcov_done
