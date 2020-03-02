086dd72fac542be995a19e55baba3de26c7a4c46
francesco <info.francescobianco@gmail.com>
2020-03-02 04:10:07 +0100

    publish

--- lcov.sh
+++ lcov.sh
@@ -28,9 +28,25 @@
 
 [[ -z "${LCOV_DEBUG}" ]] || set -x
 
-set -f
+set -ef
 
 VERSION="0.13.4a"
+
+usage () {
+    echo "Usage: ./lcov.sh [OPTION]... FILE..."
+    echo ""
+    echo "Executes FILE as a test case also collect each LCOV info and generate HTML report"
+    echo ""
+    echo "List of available options"
+    echo "  -e, --extension EXT     Coverage of every *.EXT file (default: sh)"
+    echo "  -i, --include PATH      Include files matching PATH"
+    echo "  -x, --exclude PATH      Exclude files matching PATH"
+    echo "  -o, --output OUTDIR     Write HTML output to OUTDIR"
+    echo "  -h, --help              Display this help and exit"
+    echo "  -v, --version           Display current version"
+    echo ""
+    echo "Documentation can be found at https://github.com/javanile/lcov.sh"
+}
 
 trap '$(jobs -p) || kill $(jobs -p)' EXIT
 
@@ -54,7 +70,7 @@ output=coverage
 skip_flag="${escape}[37m(skip)${escape}[0m"
 done_flag="${escape}[1m${escape}[32m(done)${escape}[0m"
 fail_flag="${escape}[1m${escape}[31m(fail)${escape}[0m"
-options=$(${getopt} -n lcov.sh -o i:e:x:o:v -l extension:,include:,exclude:,output:,version -- "$@")
+options=$(${getopt} -n lcov.sh -o i:e:x:o:vh -l extension:,include:,exclude:,output:,version,help -- "$@")
 
 eval set -- "${options}"
 
