8771b9e1cca48da97c601f84a9094a390237df9f
francescobianco <info.francescobianco@gmail.com>
2020-07-03 02:48:56 +0200

    Release

--- examples/build.sh
+++ examples/build.sh
@@ -13,19 +13,15 @@ dump() {
     echo '```'
 }
 
-cd examples
-(
-    echo  "# Examples"
-    echo  "..."
-) > index.md
-
-cd basic
+##
+cd examples/basic
 (
     cp ../../lcov.sh .
+    rm -fr coverage
     code script.sh
     code script-test.sh
     dump ./lcov.sh script-test.sh
     echo '<iframe width="100%" height="400" src="coverage/"></iframe>'
     rm lcov.sh
 ) > index.md
-cd ..
+cd ../..
